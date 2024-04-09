5c0a085e8a982b96fae790dcd4edd7da23d4d40f
ValentinaMC <valentinam675@gmail.com>
2024-04-08 20:34:13 -0500

    Project creation.

---
+++ Playlist.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Playlist.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
