20d083843bc4ed8c53a44d02c6a20b0c3da24506
ValentinaMC <valentinam675@gmail.com>
2024-04-09 00:02:03 -0500

    Created PasswordUtilities.cs in the Utilities folder.
    This class provides utilities for password encryption using the SHA256 algorithm. It is used to ensure the security of passwords stored in the database.

---
+++ Playlist.Web/Utilities/PasswordUtilities.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Playlist.Web.Utilities
+{
+    public static class PasswordUtilities
+    {
+        public static string HashPassword(string password)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                // Calculate the hash of the password
+                byte[] hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+                //Convert the bytes of the hash into a hexadecimal string. 
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < hashedBytes.Length; i++)
+                {
+                    builder.Append(hashedBytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+    }
+}
