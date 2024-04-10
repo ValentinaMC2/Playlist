a1ffdaaeb913a3445524607876dc2506f2066766
ValentinaMC <valentinam675@gmail.com>
2024-04-09 20:04:31 -0500

    This file contains the DTO (Data Transfer Object) classes for representing song data (SongDTO) and user data (UserDTO).
    These classes are used to transfer data between the presentation layer and the service layer in the Playlist.Web application.

---
+++ Playlist.Web/DTOs/SongDTO.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Playlist.Web.DTOs
+{
+    public class SongDTO
+    {
+        public int Id { get; set; }
+
+        [MaxLength(70, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres")]
+        [MinLength(1, ErrorMessage = "El campo {0} debe tener mínimo {1} caracter")]
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        public string Title { get; set; }
+
+        [MaxLength(30, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres")]
+        [MinLength(1, ErrorMessage = "El campo {0} debe tener mínimo {1} caracter")]
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        public string Genre { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "La duración debe ser un número entero positivo (Segundos)")]
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        public int DurationInSeconds { get; set; }
+
+        [Range(1900, 2024, ErrorMessage = "El año de lanzamiento debe estar entre {1} y {2}")]
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        public int ReleaseYear { get; set; }
+
+        public int UserId { get; set; } //User ID associated with the song.
+    }
+}
---
+++ Playlist.Web/DTOs/UserDTO.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Playlist.Web.DTOs
+{
+    public class UserDTO
+    { 
+        public int Id { get; set; }
+    
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "El campo {0} debe tener entre {2} y {1} caracteres")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [Range(1, 106, ErrorMessage = "La edad debe estar entre {1} y {2}")]
+        public int Age { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [StringLength(12, ErrorMessage = "El campo {0} debe tener como máximo {1} caracteres")]
+        public string Country { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [DataType(DataType.DateTime)]
+        public DateTime RegistrationDate { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [EmailAddress(ErrorMessage = "El campo {0} debe ser una dirección de correo electrónico válida")]
+        [StringLength(70, MinimumLength = 1, ErrorMessage = "El campo {0} debe tener como máximo {1} caracteres")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [StringLength(50, MinimumLength = 6, ErrorMessage = "La contraseña debe tener entre {2} y {1} caracteres")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "La contraseña y su confirmación no coinciden")]
+        [Display(Name = "Confirmar Contraseña")]
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+}
